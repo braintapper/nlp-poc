a1d880ef03fe80e7205fcff06d9f464780b5551f
Steven Ng <steven.ng@braintapper.com>
2020-03-08 13:45:32 -0400

    WIP

--- src/pages/Main/Group.svelte
+++ src/pages/Main/Group.svelte
@@ -9,16 +9,44 @@
   import compromiseNumbers from "compromise-numbers"
   import compromiseDates from "compromise-dates"
 
+  nlp.extend(compromiseNumbers)
+  nlp.extend(compromiseDates)
+
+  nlp.extend (Doc, world)->
+    world.addTags
+      Operator:
+        isNotA: "Noun"
+        isNotA: "Adjective"
+        isNotA: "Conjunction"
+    world.addTags
+      Separator:
+        isNotA: "Noun"
+        isNotA: "Adjective"
+        isNotA: "Conjunction"
+
+    world.addWords
+      "+": "Operator"
+      "*": "Operator"
+      "-": ["Operator","Separator"]
+      "/": ["Operator","Separator"]
+      "x": "Operator"
+      "plus": "Operator"
+      "minus": "Operator"
+      "times": ["Operator","Plural"]
+      "US dollars": "Currency"
+
+
 
   expressions = [
-
-
+    "10 percent off 100"
+    "5 times 1"
     "35% off 400k"
     "$30 a night x 3 nights"
     "$30CAD in USD"
     "$30CAD in US Dollars"
     "$30CAD + $20USD"
     "line1 + 5%"
+    "5 / 10"
     "line1 as a percentage of #line2"
     "line1 as a % of #line2"
     "5 days from now"
@@ -43,31 +71,27 @@
 
 </script>
 <style lang="sass">
-  field
-    position: relative
-    margin-top: 24px
   nodes
-
     display: table
+    margin-bottom: 20px
   node
     display: table-cell
     vertical-align: top
     top: 0px
     left: auto
     display: inline-block
-    margin-right: 20px
+    margin-right: 24px
+    h1
+      color: var(--blue)
+      text-decoration: underline
 </style>
 <Group title="Main">
   <div>
   {#each expressions as expression}
-    <field>
-      <label>Test Sentence</label>
-      <input type="text" value={expression}/>
-    </field>
     <nodes va="top">
       {#each nodes(expression) as item}
           <node>
-            <div><h1>{item.text} ({item.clean})</h1></div>
+            <div><h1><code>{item.text}</code></h1></div>
             <div>{@html item.tags.join("<br/>")}</div>
           </node>
       {/each}
